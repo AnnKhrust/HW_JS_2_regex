053455e7f7b03445bc737d9f52a0140971202893
Ann Kh <anna.khrust@gmail.com>
2024-07-10 00:10:07 +0300

    task completed

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,14 @@
-// TODO: write your code here
-import sum from './basic';
+export default class Validator {
+    constructor(login) {
+        this.login = login;
+    }
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+    validateUsername() {
+        const regex_1 = /^[a-z][\da-z_-]*[a-z]$/i.test(this.login);
+        const regex_2 = !(/\d{4,}/.test(this.login));
+        if (regex_1 && regex_2) {
+            return true;
+        }
+        return false;
+    }
+}
